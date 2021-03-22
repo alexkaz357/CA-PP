891109426f850ad266ee9199df7c04ef70cf6619
alexkaz357 <alexkaz357@gmail.com>
2021-03-22 13:47:26 +0200

    'Deploy'

---
+++ precache-manifest.e3cc7e8cd23ad1581ed5d55fc864a568.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d062b94e8984812854109bffc217771c",
+    "url": "/CA-PP/index.html"
+  },
+  {
+    "revision": "41fe7f5715967a5c4c00",
+    "url": "/CA-PP/static/css/main.bae53b53.chunk.css"
+  },
+  {
+    "revision": "8d41091f226c89deb45b",
+    "url": "/CA-PP/static/js/2.c237b45e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CA-PP/static/js/2.c237b45e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41fe7f5715967a5c4c00",
+    "url": "/CA-PP/static/js/main.5d1ae61e.chunk.js"
+  },
+  {
+    "revision": "878a42f4d42905aa9f93",
+    "url": "/CA-PP/static/js/runtime-main.548be8b4.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/CA-PP/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "34e9c32dce94acabff44722c69666a89",
+    "url": "/CA-PP/static/media/hero-bg-desktop.34e9c32d.jpg"
+  },
+  {
+    "revision": "7d046a9f1ef44bc0791fe15aac313a9a",
+    "url": "/CA-PP/static/media/hero-bg-mobile.7d046a9f.jpg"
+  },
+  {
+    "revision": "932ec5bc8d11397b3eca4d28efa9221b",
+    "url": "/CA-PP/static/media/logo.932ec5bc.svg"
+  },
+  {
+    "revision": "c271854d6e7bcdd0e6d503d1668337de",
+    "url": "/CA-PP/static/media/menu-item1.c271854d.jpg"
+  },
+  {
+    "revision": "a1a68d47dc293a4690a63d7169d300a8",
+    "url": "/CA-PP/static/media/menu-item2.a1a68d47.jpg"
+  },
+  {
+    "revision": "9c7e1cb0e64389ee1b66cd04d75c7fc7",
+    "url": "/CA-PP/static/media/menu-item3.9c7e1cb0.jpg"
+  },
+  {
+    "revision": "c2c518b398ec0dd6a19b3eb06925a683",
+    "url": "/CA-PP/static/media/pattern-divide.c2c518b3.svg"
+  }
+]);
